1190749bf4143fe9693ed2f74f38019822fcecf3
Gornicki, Lukasz <lukasz.gornicki@hybris.com>
2016-12-30 23:16:37 +0100

    /issues/2
    opt doesn’t have to be passed any more. Also solved issue with breaking
    the flow because of some broken dirs that fs cannot read

--- src/index.js
+++ src/index.js
@@ -7,40 +7,49 @@ let result = {};
 
 async function walkDir(path, opt){
 
-  const pathStats = await fsOps.readInfo(path);
+  try {
 
-  if (pathStats.isFile()) return true;
+    const pathStats = await fsOps.readInfo(path);
 
-  const dirContent = await fsOps.readDir(path);
+    if (pathStats.isFile()) return true;
+
+    const dirContent = await fsOps.readDir(path);
 
     for (let i = 0; i < dirContent.length; i++) {
 
-    const name = dirContent[i];
-    const contentPath = `${path}/${name}`;
+      const name = dirContent[i];
+      const contentPath = `${path}/${name}`;
 
-    if (opt && opt.blDir && objOps.isBlacklisted(path, opt.blDir)) break;
+      if (opt && opt.blDir && objOps.isBlacklisted(path, opt.blDir)) break;
 
-    let pathResult = result[path];
-    const isFile = await walkDir(contentPath, opt);
+      let pathResult = result[path];
+      const isFile = await walkDir(contentPath, opt);
 
-    if (isFile === true && opt && opt.blFiles && !objOps.isBlacklisted(name, opt.blFiles)){
+      if (isFile === true){
 
-      if (pathResult) {
+        if (opt && opt.blFiles && objOps.isBlacklisted(name, opt.blFiles)) break;
 
-        result[path] = ++pathResult;
-        //for listing files instead of counting them
-        //pathResult.push(name)
-        //result[path] = pathResult;
-      } else {
+        if (pathResult) {
 
-        result[path] = 1;
-        //for listing files instead of counting them
-        //result[path] = [name];
+          result[path] = ++pathResult;
+          //for listing files instead of counting them
+          //pathResult.push(name)
+          //result[path] = pathResult;
+        } else {
+
+          result[path] = 1;
+          //for listing files instead of counting them
+          //result[path] = [name];
+        }
       }
     }
-  }
 
-  return result;
+    return result;
+  }
+  catch(err) {
+
+    console.log('Read error', err);
+  }
 }
 
 module.exports = walkDir;
